a0d201e803bef84aa3bdfe03090f8b33221bae0f
Tomeu Pons <tomeu.pons@txerpa.com>
2022-08-02 12:45:41 +0200

    refactor files requirements

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 from facturae import __version__
 
-with open("requirements.txt", "r") as f:
+with open("requirements/requirements.txt", "r") as f:
     INSTALL_REQUIRES = f.readlines()
 
-with open("requirements-dev.txt", "r") as f:
+with open("requirements/requirements-dev.txt", "r") as f:
     TESTS_REQUIRES = f.readlines()
 
 setup(
@@ -17,14 +17,17 @@ setup(
     author_email="devel@gisce.net",
     original_author="Electrica Sollerense, S.A.U.",
     original_author_email="informatica@el-gas.es",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests*"]),
     install_requires=INSTALL_REQUIRES,
     tests_require=TESTS_REQUIRES,
     license="GPLv3",
     description="Facturae",
     long_description=open("README.md").read(),
     classifiers=[
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
     ],
 )
